081bc7cb60dbac1f7cd028a05e5d14ec192e2f50
Yunho Cho <yhjc2692@gmail.com>
2024-03-09 14:24:11 -0500

    Explicitly supply 'self' when calling ort_validate

--- test_all.py
+++ test_all.py
@@ -146,7 +146,7 @@ class ONNXExporterTester(unittest.TestCase):
                 test_ouputs = model(*test_inputs)
                 if isinstance(test_ouputs, torch.Tensor):
                     test_ouputs = (test_ouputs,)
-            self.ort_validate(onnx_io=onnx_io, inputs=test_inputs, outputs=test_ouputs, tolerate_small_mismatch=tolerate_small_mismatch)
+            self.ort_validate(self=self, onnx_io=onnx_io, inputs=test_inputs, outputs=test_ouputs, tolerate_small_mismatch=tolerate_small_mismatch)
 
     def ort_validate(self, onnx_io, inputs, outputs, tolerate_small_mismatch=False):
 
